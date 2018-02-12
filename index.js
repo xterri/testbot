20ddc39dc64be64bcd877be815c3df68063b4e2d
xterri <thuynh1989@gmail.com>
2018-02-12 14:06:03 -0700

    changed port

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.set('port', process.env.PORT || 3000);
 
-app.listen(process.env.PORT || 3000, () => console.log("Server listening on port " + app.get('port'));
+app.listen(app.get('port'), () => console.log("Server listening on port " + app.get('port'));
 
 const verificationController = require('./controllers/verification');
 const messageWebhookController = require('./controllers/messageWebhook');
