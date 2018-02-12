ac59e92754f2c20d73dbc6836a2b7ead3c00dc63
xterri <thuynh1989@gmail.com>
2018-02-11 20:00:42 -0700

    edited texts

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-// using Express's server
-const app = express();
 // set up the server
+const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
