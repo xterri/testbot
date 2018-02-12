8d0b4087e3633dc976f7b53cf2c076c6f7259989
xterri <thuynh1989@gmail.com>
2018-02-11 17:13:05 -0700

    change bot message for heroku

--- index.js
+++ index.js
@@ -8,3 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(3000, () => console.log("Server listening on port 3000"));
+
+app.get('/', function(req, res) {
+	res.send("New bot. who dis?");
+});
