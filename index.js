da94783829389f75044f77000d72d8935b0d41c8
xterri <thuynh1989@gmail.com>
2018-02-12 14:03:58 -0700

    changed port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(process.env.PORT || 3000, () => console.log("Server listening on port 3000"));
+app.listen(process.env.PORT || 3000, () => console.log("Server listening on port " + app.get('port'));
 
 const verificationController = require('./controllers/verification');
 const messageWebhookController = require('./controllers/messageWebhook');
