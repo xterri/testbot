993478180eaa11a46c199b199a6408e4dcbb7ab4
xterri <thuynh1989@gmail.com>
2018-02-14 16:48:57 -0700

    trying to fix echo issue

--- helpers/processMessage.js
+++ helpers/processMessage.js
@@ -4,22 +4,24 @@ const apiAiClient = require('apiai')(API_AI_TOKEN);
 const sendTextMessage = require('./sendTextMessage');
 
 module.exports = (event) => {
-    // takes message recieved and returns proper response
-    const senderId = event.sender.id;
-    const message = event.message.text;
+    if (!event.message.is_echo) {
+        // takes message recieved and returns proper response
+        const senderId = event.sender.id;
+        const message = event.message.text;
 
-    console.log(senderId);
-    // sends user messages to API.ai
-    // will need to change "sessionId" to identify other users
-    const apiaiSession = apiAiClient.textRequest(message, {sessionId: senderId});
+        console.log(senderId);
+        // sends user messages to API.ai
+        // will need to change "sessionId" to identify other users
+        const apiaiSession = apiAiClient.textRequest(message, {sessionId: senderId});
 
-    // get and return response message to user
-    apiaiSession.on('response', (response) => {
-        const result = response.result.fulfillment.speech;
-        console.log(result);
-        sendTextMessage(senderId, result);
-    });
+        // get and return response message to user
+        apiaiSession.on('response', (response) => {
+            const result = response.result.fulfillment.speech;
+            console.log(result);
+            sendTextMessage(senderId, result);
+        });
 
-    apiaiSession.on('error', error => console.log(error));
-    apiaiSession.end();
+        apiaiSession.on('error', error => console.log(error));
+        apiaiSession.end();
+    }
 };
